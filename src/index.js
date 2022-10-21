91a4830e37603577799c25a026f2917264068b50
jatan-ops <jatan.shah90@gmail.com>
2022-10-21 23:18:00 +0530

    added button for rendering

--- src/index.js
+++ src/index.js
@@ -36,8 +36,9 @@ class AlignmentBlockTune {
         if(this.data.alignment) {
             this.wrapper.classList.toggle('ce-tune-alignment--center')
         }    
-        blockContent.addEventListener('keyup',(e) => {
-            if(e.altKey && e.key === 'i') {                
+        blockContent.addEventListener('keydown',(e) => {
+            if(e.key === 'Tab') {                            
+                e.preventDefault()
                 const toggle = this.wrapper.classList.toggle('ce-tune-alignment--center')
                 this.data = {
                     alignment: toggle
@@ -52,7 +53,25 @@ class AlignmentBlockTune {
      * @returns {*}
      */
     render() {
-        return null;
+        const wrapper = make("div");
+
+        const button = document.createElement('button');
+        button.classList.add(this.api.styles.settingsButton)
+        button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/></svg>'
+        button.type = 'button';
+
+        button.classList.toggle(this.api.styles.settingsButtonActive,this.data.alignment);
+
+        button.addEventListener('click',() => {
+            const toggle = this.wrapper.classList.toggle('ce-tune-alignment--center')
+            this.data = {
+                alignment: toggle
+            }            
+            button.classList.toggle(this.api.styles.settingsButtonActive, this.data.alignment);
+        })
+
+        wrapper.appendChild(button);
+        return wrapper
     }
     /**
      * save
